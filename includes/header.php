e4d938470cb0f779349a46231d476271bdd67b65
loulouli-poopy <issintyasaid.com>
2026-06-24 17:19:26 +0200

    creation header, footer et index

--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>colocation</title>
+  <link rel="stylesheet" href="style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+   <nav class="navbar fixed-top bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Coloc Vienne</a>
+    <a class="navbar-brand" href="#">Colocation Vienne</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,10 +23,16 @@
           <a class="nav-link active" aria-current="page" href="#">acceuil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Rechercher une colocation</a>
+          <a class="nav-link " href="#">Rechercher une colocation</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">connexion</a>
+          <a class="nav-link " href="#">Publier une annonce</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link " href="#">Rechercher une colocation</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link " href="#">connexion</a>
         </li>
       </ul>
     </div>
--- index.php
+++ index.php
@@ -2,8 +2,54 @@
 include("includes/header.php");
 
 ?>
+    <h1>VIVEZ MIEUX, ENSEMBLE</h1>
+    <div><p>l'application de colocation qui privilégie les affinités
+     humaines avant les mètres carrés trouver l'espace qui vous ressemble</p>
+    </div>
+    <main>
     
-<p>  le contenu de l'index</p>
+
+ <nav class="navbar bg-body-tertiary w-25">
+  <div class="container-fluid">
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="localisation" aria-label="Search"/>
+      <input class="form-control me-2" type="search" placeholder="prix cible" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+</section>
+
+
+      <section>
+        <h4>dernières opportunités</h4>
+        <p>c'est des lieux de des lieux de vie sélectionnés 
+        pour leurs esprits communautaires</p>
+
+
+       <!-- nos ilage serons ici -->
+       <figure class="figure">
+                <img src="" class="figure-img img-fluid rounded img-thumbnail" alt="...">
+                <figcaption class="figure-caption text-light"></figcaption>
+            </figure>
+
+      </section>
+      
+      <section class="bg-success">
+       <h2>Ne cherchez plus un appartement, trouvez vos futurs amis.</h2>
+        
+       <ol>
+        <li>Algorithme de compatibilité basé sur le mode de vie (rythme, ménage, invités)</li>
+        <li>Messagerie sécurisée pour faire connaissance avant la visite.</li>
+        <li>Gestion simplifiée des cautions et des documents administratifs.</li>
+       </ol>
+
+
+
+      </section>
+
+
+    </main>
 
 <?php 
 include("includes/footer.php");
