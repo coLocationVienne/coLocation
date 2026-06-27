888babac775a7833b5d6cbc02191408a239ddba3
srohid <rohid.safi@opngo.com>
2026-06-27 14:56:02 +0200

    bugfix_09_01_Correction de l'organisation des fichiers et de la traduction de la page de connexion.

--- index.php
+++ index.php
@@ -11,26 +11,11 @@ include("includes/header.php");
      humaines avant les mètres carrés trouver l'espace qui vous ressemble</p>
     </div>
 </div>
-
- <nav class="navbar bg-body-tertiary w-25">
-  <div class="container-fluid">
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="localisation" aria-label="Search"/>
-      <input class="form-control me-2" type="search" placeholder="prix cible" aria-label="Search"/>
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
 </section>
-
-
       <section>
         <h4>dernières opportunités</h4>
         <p>c'est des lieux de des lieux de vie sélectionnés 
         pour leurs esprits communautaires</p>
-
-
-       <!-- nos image seront ici -->
        <figure class="figure">
                 <img src="https://media.istockphoto.com/photos/stylish-living-room-interior-with-beautiful-house-plants-picture-id1312439845?k=20&m=1312439845&s=170667a&w=0&h=Dm0vTngpk4UP8Zh80DxaDoOAl1IdHxegakHnFlV709o=" class="figure-img img-fluid rounded img-thumbnail" alt="...">
                 <figcaption class="blockquote-footer">245$</figcaption>
@@ -47,20 +32,12 @@ include("includes/header.php");
             </figure>
 
       </section>
-      
       <section class="bg-secondary">
        <h2 class="text-white">Ne cherchez plus un appartement, trouvez vos futurs amis.</h2>
-        
-       <span class="nombre-cercle">1</span> <p class="text-white">Algorithme de compatibilité basé sur le mode de vie (rythme, ménage, invités)</p>
-       <span class="nombre-cercle">2</span> <p class="text-white">Messagerie sécurisée pour faire connaissance avant la visite.</p>
-       <span class="nombre-cercle">3</span> <p class="text-white">Gestion simplifiée des cautions et des documents administratifs.</p>
-       
-
-
-
+        <span class="nombre-cercle">1</span> <p class="text-white">Algorithme de compatibilité basé sur le mode de vie (rythme, ménage, invités)</p>
+        <span class="nombre-cercle">2</span> <p class="text-white">Messagerie sécurisée pour faire connaissance avant la visite.</p>
+        <span class="nombre-cercle">3</span> <p class="text-white">Gestion simplifiée des cautions et des documents administratifs.</p>
       </section>
-
-
     </main>
 
 <?php 
