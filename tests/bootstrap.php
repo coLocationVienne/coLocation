1317d9345f410ff889976d5df46648810ee785f2
Mohammad Rohid safi <sroheed@gmail.com>
2026-07-28 14:22:05 +0200

    23_01 phpunit test est ajouter (#39)
    Co-authored-by: srohid <rohid.safi@opngo.com>

---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/../app/Core/Autoloader.php';
+
+// Define any constants needed for tests
+if (!defined('COLOCATION_INIT_LOADED')) {
+    define('COLOCATION_INIT_LOADED', true);
+}
