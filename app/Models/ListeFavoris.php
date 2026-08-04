4cdc670599a4a8d8cda04deb7edda5dd172f3671
loulouli-poopy <issintyasaid@gmail.com>
2026-08-04 16:11:08 +0200

    liste_favoris class

---
+++ app/Models/ListeFavoris.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+class ListeFavoris {
+    private ?int $id_listeFavoris;
+    private string $titre_liste;
+    private ?int $id_utilisateur;
+
+    public function __construct(array $data){
+        $this->id_listeFavoris = isset($data['id_liste']) ? (int)$data['id_liste'] : null;
+        $this->titre_liste = $data['titre_liste'] ?? '';
+        $this->id_utilisateur = (int)($data['id_utilisateur'] ?? 0);
+
+    }
+     
+
+    //getters
+        public function getIdListe(): ?int { return $this->id_liste; }
+        public function getNomListe(): string { return $this->nom_liste; }
+        public function getIdUtilisateur(): int { return $this->id_utilisateur; }
+
+
+
+    //setters
+    public function setIdListeFavoris(?int $id_listeFavoris): self
+    {$this->id_listeFavoris = $id_listeFavoris; 
+    return $this;
+    }
+    public function setTitreListe(string $titre_liste): self
+    {$this->titre_liste = $titre_liste;
+    return $this;
+    }
+   
+}
+
+
---
+++ app/Models/ListeFavorisDAO.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\DAO;
+use App\Models\ListeFavoris;
+
+class ListeFavorisDAO extends DAO {
+    public function __construct() {
+        parent::__construct('liste_favoris', 'id_liste');
+    }
+
+    protected function hydrate(array $row): ListeFavoris {
+        return new ListeFavoris($row);
+    }
+
+    protected function dehydrate(object $listeFavoris): array {
+        /** @var ListeFavoris $listeFavoris */
+        return [
+            'id_liste' => $listeFavoris->getIdListe(),
+            'titre_liste' => $listeFavoris->getNomListe(),
+            'id_utilisateur' => $listeFavoris->getIdUtilisateur(),
+        ];
+    }
+
+    public function getListesByUserId(int $userId): array {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id_utilisateur = :id_utilisateur";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id_utilisateur', $userId, \PDO::PARAM_INT);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+
+        $listes = [];
+        foreach ($results as $row) {
+            $listes[] = $this->hydrate($row);
+        }
+        return $listes;
+    }
+
+    public function getListeByIdAndUserId(int $listeId, int $userId): ?ListeFavoris {
+        $query = "SELECT * FROM " . $this->table_name . " WHERE id_liste = :id_liste AND id_utilisateur = :id_utilisateur";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id_liste', $listeId, \PDO::PARAM_INT);
+        $stmt->bindParam(':id_utilisateur', $userId, \PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        return $result ? $this->hydrate($result) : null;
+    }
+}
