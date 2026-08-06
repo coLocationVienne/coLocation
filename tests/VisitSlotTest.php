23eb6af1e33c2f856b51dfdcfefe97e82d93101b
Mohammad Rohid safi <sroheed@gmail.com>
2026-08-06 15:46:06 +0200

    ECF deplyment (#50)
    * testUnitaire commentaire
    
    * ajouter docker est test integration (#41)
    
    * ajouter docker est test integration
    
    * verif bug
    
    * verif bug fix
    
    * verif bug
    
    * verif bug
    
    * verif bug
    
    * verif bug
    
    * verif bug
    
    * verif bug
    
    * verif bug
    
    * verif bug
    
    * fixed the docker issue and added dockerfile.db to enhance le speed for it.
    
    * base de donee modifier
    
    * fixed le tests
    
    * test work flow
    
    * mise a jour base de donnee
    
    * fixed intregration tests
    
    * added health check docker
    
    ---------
    
    Co-authored-by: srohid <rohid.safi@opngo.com>
    Co-authored-by: loulouli-poopy <issintyasaid.com>
    Co-authored-by: loulouli-poopy <issintyasaid@gmail.com>
    
    * 27 01 rdv (#44)
    
    * ajouter le fouctionality pour le utilisateur de envoyer mail quan il son pas en line
    
    * RDV est ajouter
    
    * fixed bugs for the unit test and interation test issue
    
    ---------
    
    Co-authored-by: srohid <rohid.safi@opngo.com>
    
    * fixed the db rest issues and remove un wanted init load (#45)
    
    * fixed the db rest issues and remove un wanted init load
    
    * fixed CI githug
    
    * checking
    
    * dd
    
    * jjj
    
    * dd
    
    * Fix CI test failures: improve SQL splitting in bootstrap and allow DB name overrides
    
    ---------
    
    Co-authored-by: srohid <rohid.safi@opngo.com>
    Co-authored-by: sroheed <2.0150652e+07+sroheed@users.noreply.github.com>
    
    * fonctionnalité creer des listes en favoris
    
    * 27_02 unit test for planfication and integration test is added (#48)
    
    * 27_02 unit test for planfication and integration test is added
    
    * test fixed
    
    ---------
    
    Co-authored-by: srohid <rohid.safi@opngo.com>
    
    * test unitaiter liste favoris dao
    
    ---------
    
    Co-authored-by: loulouli-poopy <issintyasaid.com>
    Co-authored-by: issintya <issintyasaid@gmail.com>
    Co-authored-by: srohid <rohid.safi@opngo.com>
    Co-authored-by: sroheed <2.0150652e+07+sroheed@users.noreply.github.com>

---
+++ tests/VisitSlotTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\VisitSlot;
+
+class VisitSlotTest extends TestCase {
+    private array $slotData;
+
+    protected function setUp(): void {
+        $this->slotData = [
+            'id_creneauVisite' => 1,
+            'date_visite' => '2026-08-10',
+            'heure_debut' => '14:00:00',
+            'heure_fin' => '15:00:00',
+            'nb_personne_max' => 5,
+            'id_annonce' => 10
+        ];
+    }
+
+    public function testConstructorAndGetters(): void {
+        $slot = new VisitSlot($this->slotData);
+
+        $this->assertEquals(1, $slot->getId());
+        $this->assertEquals('2026-08-10', $slot->getDateVisite());
+        $this->assertEquals('14:00:00', $slot->getHeureDebut());
+        $this->assertEquals('15:00:00', $slot->getHeureFin());
+        $this->assertEquals(5, $slot->getNbPersonneMax());
+        $this->assertEquals(10, $slot->getIdAnnonce());
+    }
+
+    public function testSetters(): void {
+        $slot = new VisitSlot([]);
+
+        $slot->setId(2);
+        $slot->setDateVisite('2026-08-11');
+        $slot->setHeureDebut('10:00:00');
+        $slot->setHeureFin('11:00:00');
+        $slot->setNbPersonneMax(3);
+        $slot->setIdAnnonce(20);
+
+        $this->assertEquals(2, $slot->getId());
+        $this->assertEquals('2026-08-11', $slot->getDateVisite());
+        $this->assertEquals('10:00:00', $slot->getHeureDebut());
+        $this->assertEquals('11:00:00', $slot->getHeureFin());
+        $this->assertEquals(3, $slot->getNbPersonneMax());
+        $this->assertEquals(20, $slot->getIdAnnonce());
+    }
+}
